f0e3522e7baf1fb974f1c014d89366e31e213b9b
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-10 13:54:04 -0600

    styles: fix Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ buttonText }) {
     return (
-        <button href="#_" className="relative inline-block my-2 px-4 py-2 font-medium  shadow1 group">
+        <button href="#_" className="relative inline-block px-4 py-2 font-medium  shadow1 group">
             <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-magenta1 group-hover:translate-x-0 group-hover:-translate-y-1 group-active:translate-y-0"></span>
             <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-blue1 group-hover:-translate-x-1 group-hover:translate-y-0 group-active:translate-x-0"></span>
             <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
