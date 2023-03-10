59567f96c72eb0f7691ed8a88e082384d2422736
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-10 13:30:12 -0600

    styles: Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,11 @@
 export default function Button({ buttonText }) {
     return (
-        <div className="mx-2 mt-2">
-            
-            <button href="#_" className="relative inline-block my-2 px-4 py-2 font-medium  shadow1 group">
-                <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-magenta1 group-hover:translate-x-0 group-hover:-translate-y-1 group-active:translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-blue1 group-hover:-translate-x-1 group-hover:translate-y-0 group-active:translate-x-0"></span>
-                <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black transition duration-100 ease-in group-active:border-2 group-active:border-white"></span>
-                <span className="relative text-black group-hover:text-white">{buttonText}</span>
-            </button>  
-        </div>
+        <button href="#_" className="relative inline-block my-2 px-4 py-2 font-medium  shadow1 group">
+            <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-magenta1 group-hover:translate-x-0 group-hover:-translate-y-1 group-active:translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-blue1 group-hover:-translate-x-1 group-hover:translate-y-0 group-active:translate-x-0"></span>
+            <span className="absolute inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black transition duration-100 ease-in group-active:border-2 group-active:border-white"></span>
+            <span className="relative text-black group-hover:text-white">{buttonText}</span>
+        </button>
     );
 }
