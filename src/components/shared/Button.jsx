d80729ab441dea16feca92671dbd3803d34e9e2c
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-12 12:49:37 -0600

    moved components in new dir

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,14 @@
+export default function Button({ buttonText }) {
+    return (
+        <>
+        <button href="#_" className="relative inline-block px-4 py-2 font-medium group">
+            <span className="absolute rounded-lg inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-magenta1 group-hover:translate-x-0 group-hover:-translate-y-1 group-active:translate-y-0"></span>
+            <span className="absolute rounded-lg inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-blue1 group-hover:-translate-x-1 group-hover:translate-y-0 group-active:translate-x-0"></span>
+            <span className="absolute rounded-md inset-0 w-full h-full transition duration-100 ease-in transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute rounded-md inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black transition duration-100 ease-in"></span>
+            <span className="relative text-black group-hover:text-white"><p className="group-active:multicolortext">Search</p></span>
+        </button>
+        
+        </>
+    );
+}
--- src/components/shared/InputSelect.jsx
+++ src/components/shared/InputSelect.jsx
@@ -2,7 +2,7 @@ export default function InputSelect({options, label, id}) {
     return (
         <div className="w-full">
             <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-            <select id={id} className="bg-gray-50 border border-black text-gray-900 text-sm focus:ring-black focus:border-black block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id={id} className="bg-gray-50 rounded border border-gray-500 text-gray-900 text-sm focus:ring-black focus:border-black block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 {options.map(option => (
                     <option key={option.value} value={option.value}>{option.viewValue}</option>
                 ))}
