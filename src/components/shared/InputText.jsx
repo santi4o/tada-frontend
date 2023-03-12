49ff2b93d27842840c8984ec2a0b541be473394b
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-12 12:50:03 -0600

    two input text components

---
+++ src/components/shared/InputText.jsx
@@ -0,0 +1,22 @@
+export default function InputText({ label, placeholder }) {
+  return (
+    <div className="flex items-center">
+      {label && (
+        <label
+          htmlFor="floating_email"
+          className="mr-2 text-gray-900 dark:text-gray-400 bg-white peer-focus:dark:text-blue-500"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        type="text"
+        name="floating_email"
+        id="floating_email"
+        className="block rounded-md w-full py-2.5 pr-2 text-sm text-gray-900 bg-transparent border border-gray-500 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-black focus:border-black  focus:placeholder-gray-500 peer"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
