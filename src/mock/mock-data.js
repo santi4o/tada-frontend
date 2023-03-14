559a623d329de6a297e242c0eb8b583f62a5fe9a
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-14 13:35:03 -0600

    mock data

---
+++ src/mock/mock-data.js
@@ -0,0 +1,65 @@
+export const TEST_DATA = {
+    "content": [
+        {
+            "id": 1,
+            "text": "finish front BASIC functionality",
+            "priority": 0,
+            "creationDate": "2011-03-22T10:44:46",
+            "dueDate": "2011-04-02T10:44:46",
+            "done": false,
+            "doneDate": null
+        },
+        {
+            "id": 2,
+            "text": "write missing API endpoints",
+            "priority": 0,
+            "creationDate": "2011-03-22T10:44:46",
+            "dueDate": null,
+            "done": false,
+            "doneDate": null
+        },
+        {
+            "id": 3,
+            "text": "finish basic api endpoints",
+            "priority": 1,
+            "creationDate": "2011-03-22T10:44:46",
+            "dueDate": null,
+            "done": true,
+            "doneDate": null
+        },
+        {
+            "id": 4,
+            "text": "start calling api",
+            "priority": 2,
+            "creationDate": "2011-03-22T10:44:46",
+            "dueDate": null,
+            "done": false,
+            "doneDate": null
+        }
+    ],
+    "pageable": {
+        "sort": {
+            "empty": false,
+            "unsorted": false,
+            "sorted": true
+        },
+        "offset": 0,
+        "pageNumber": 0,
+        "pageSize": 2,
+        "paged": true,
+        "unpaged": false
+    },
+    "totalPages": 2,
+    "totalElements": 3,
+    "last": false,
+    "size": 2,
+    "number": 0,
+    "sort": {
+        "empty": false,
+        "unsorted": false,
+        "sorted": true
+    },
+    "numberOfElements": 2,
+    "first": true,
+    "empty": false
+}
