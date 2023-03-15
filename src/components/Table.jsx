e154fa68aea2f7b61b672f740c60798b42fdf620
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-14 23:56:36 -0600

    updated table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,7 @@
 import { TASK_TABLE_PROPS } from "../config/task-properties";
-import { TEST_DATA } from "../mock/mock-data.js";
 import TableRow from "./TableRow";
 
-export default function Table() {
+export default function Table({ tasks }) {
     const sortIcon = (
         <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
     );
@@ -12,7 +11,7 @@ export default function Table() {
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        {TASK_TABLE_PROPS.map((prop) => (
+                        {TASK_TABLE_PROPS.map(prop => (
                             <th
                                 key={prop.value}
                                 scope="col"
@@ -31,7 +30,7 @@ export default function Table() {
                 </thead>
 
                 <tbody>
-                    {TEST_DATA.content.map((task) => (
+                    {tasks.map(task => (
                         <TableRow key={task.id} task={task}></TableRow>
                     ))}
                 </tbody>
