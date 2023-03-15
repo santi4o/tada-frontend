887ea5887fd8644f4f5bc3fcf4857dfb455a2962
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-15 00:10:19 -0600

    formatting (tabs)

--- src/store/ThemeProvider.jsx
+++ src/store/ThemeProvider.jsx
@@ -2,40 +2,40 @@ import ThemeContext from "./theme-context";
 import { useState, useEffect } from "react";
 
 export default function ThemeProvider({ children }) {
-    const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(false);
 
-    useEffect(() => {
-        if (
-            localStorage.getItem("color-theme") === "dark" ||
-            (!("color-theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            setDarkTheme(true);
-        } else {
-            setDarkTheme(false);
-        }
-    }, []);
-
-    function handleToggleTheme() {
-        console.log("hola");
-        if (darkTheme) {
-            localStorage.setItem("color-theme", "light");
-            setDarkTheme(false);
-        } else {
-            localStorage.setItem("color-theme", "dark");
-            setDarkTheme(true);
-        }
-    }
-
-    if (darkTheme) {
-        document.body.style.backgroundColor = "black";
+  useEffect(() => {
+    if (
+      localStorage.getItem("color-theme") === "dark" ||
+      (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setDarkTheme(true);
     } else {
-        document.body.style.backgroundColor = "white";
+      setDarkTheme(false);
     }
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{ dark: darkTheme, handleToggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  function handleToggleTheme() {
+    console.log("hola");
+    if (darkTheme) {
+      localStorage.setItem("color-theme", "light");
+      setDarkTheme(false);
+    } else {
+      localStorage.setItem("color-theme", "dark");
+      setDarkTheme(true);
+    }
+  }
+
+  if (darkTheme) {
+    document.body.style.backgroundColor = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+
+  return (
+    <ThemeContext.Provider value={{ dark: darkTheme, handleToggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
