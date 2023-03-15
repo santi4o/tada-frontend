5f00c418b6c12cf9753fd95a4ae05ec7a6c10223
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-14 23:58:03 -0600

    refactor: theme logic to context provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,32 @@ import Filters from "./components/Filters";
 import Table from "./components/Table";
 import Button from "./components/shared/Button";
 import PageNav from "./components/PageNav";
-import { useEffect, useState } from "react";
+import TasksProvider from "./store/TasksProvider";
+
+import { TEST_DATA } from "./mock/mock-data.js";
+import { useContext } from "react";
+import ThemeContext from "./store/theme-context";
 
 function App() {
-    const [darkTheme, setDarkTheme] = useState(false);
-
-    useEffect(() => {
-        if (
-            localStorage.getItem("color-theme") === "dark" ||
-            (!("color-theme" in localStorage) &&
-                window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-            setDarkTheme(true);
-        } else {
-            setDarkTheme(false);
-        }
-    }, []);
-
-    function handleToggleTheme() {
-        if (darkTheme) {
-            localStorage.setItem("color-theme", "light");
-            setDarkTheme(false);
-        } else {
-            localStorage.setItem("color-theme", "dark");
-            setDarkTheme(true);
-        }
-    }
-
-    if (darkTheme) {
-        document.body.style.backgroundColor = "black";
-    } else {
-        document.body.style.backgroundColor = "white";
-    }
+    const ctx = useContext(ThemeContext)
     return (
-        <div className={"App" + (darkTheme ? " dark " : " ") + "min-h-screen"}>
+        <div className={"App" + (ctx.dark ? " dark " : " ") + "min-h-screen"}>
             {/* <div className="dark:bg-black">hola</div> */}
-            <Layout toggleThemeEvent={handleToggleTheme}>
-                <div className="md:w-5/6 lg:max-w-3xl mx-auto">
-                    <Filters></Filters>
-                </div>
-                <div className="md:w-5/6 lg:max-w-3xl mx-auto">
-                    <div className="my-4">
-                        <Button buttonText="New To Do"></Button>
+            <Layout toggleThemeEvent={ctx.handleToggleTheme}>
+                <TasksProvider>
+                    <div className="md:w-5/6 lg:max-w-3xl mx-auto">
+                        <Filters></Filters>
                     </div>
-                    <Table></Table>
-                </div>
-                <div className="mt-4 md:w-5/6 lg:max-w-3xl mx-auto flex justify-center">
-                    <PageNav ></PageNav>
-                </div>
+                    <div className="md:w-5/6 lg:max-w-3xl mx-auto">
+                        <div className="my-4">
+                            <Button buttonText="New To Do"></Button>
+                        </div>
+                        <Table tasks={[]}></Table>
+                    </div>
+                    <div className="mt-4 md:w-5/6 lg:max-w-3xl mx-auto flex justify-center">
+                        <PageNav></PageNav>
+                    </div>
+                </TasksProvider>
             </Layout>
         </div>
     );
---
+++ src/store/ThemeProvider.jsx
@@ -0,0 +1,41 @@
+import ThemeContext from "./theme-context";
+import { useState, useEffect } from "react";
+
+export default function ThemeProvider({ children }) {
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    useEffect(() => {
+        if (
+            localStorage.getItem("color-theme") === "dark" ||
+            (!("color-theme" in localStorage) &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ) {
+            setDarkTheme(true);
+        } else {
+            setDarkTheme(false);
+        }
+    }, []);
+
+    function handleToggleTheme() {
+        console.log("hola");
+        if (darkTheme) {
+            localStorage.setItem("color-theme", "light");
+            setDarkTheme(false);
+        } else {
+            localStorage.setItem("color-theme", "dark");
+            setDarkTheme(true);
+        }
+    }
+
+    if (darkTheme) {
+        document.body.style.backgroundColor = "black";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+
+    return (
+        <ThemeContext.Provider value={{ dark: darkTheme, handleToggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
