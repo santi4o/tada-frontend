0bdf9c6035d8f6717058f5a7659c88569c8e0b37
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-20 12:32:34 -0600

    feat: clear filters button

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -17,10 +17,17 @@ export default function Filters() {
     tasksContext.updateFilters([
       { property: "name", value: nameInputRef.current.value },
       { property: "priority", value: prioritySelectRef.current.value },
-      { property: "done", value: statusSelectRef.current.value }      
+      { property: "done", value: statusSelectRef.current.value },
     ]);
   }
-  
+
+  function handleClear() {
+    nameInputRef.current.value = "";
+    prioritySelectRef.current.value = -1;
+    statusSelectRef.current.value = "";
+    tasksContext.updateFilters([]);
+  }
+
   let searchSvg = (
     <path
       strokeLinecap="round"
@@ -31,36 +38,58 @@ export default function Filters() {
   );
 
   return (
-    <div className="mx-auto w-full p-4 border border-gray-100 bg-white rounded-md shadow-lg">
+    <div className="mx-auto w-full p-4 border border-gray-100 bg-white rounded-md shadow-lg dark:bg-gray-800 dark:border-gray-700">
       <div className="table">
         <div className="table-row-group">
           <div className="table-row">
-            <label htmlFor="" className="table-cell">
+            <label htmlFor="" className="table-cell dark:text-white">
               Name
             </label>
             <div className="table-cell w-full">
-              <InputText myRef={nameInputRef} placeholder="Search for to-dos containing this words"></InputText>
+              <InputText
+                myRef={nameInputRef}
+                placeholder="Search for to-dos containing this words"
+              ></InputText>
             </div>
           </div>
           <div className="table-row">
-            <label className="table-cell pr-2" htmlFor="">
+            <label className="table-cell pr-2 dark:text-white" htmlFor="">
               Priority
             </label>
             <div className="flex my-2">
               <div className="w-full sm:w-fit">
-                <InputSelect id="priority" options={PRIORITIES} myRef={prioritySelectRef} />
+                <InputSelect
+                  id="priority"
+                  options={PRIORITIES}
+                  myRef={prioritySelectRef}
+                />
               </div>
             </div>
           </div>
           <div className="table-row">
-            <label className="table-cell w-8" htmlFor="">
+            <label className="table-cell w-8 dark:text-white" htmlFor="">
               Status
             </label>
             <div className="flex flex-col sm:flex-row justify-between md:items-end">
               <div>
-                <InputSelect id="status" options={DONE_STATUS} myRef={statusSelectRef} />
+                <InputSelect
+                  id="status"
+                  options={DONE_STATUS}
+                  myRef={statusSelectRef}
+                />
               </div>
-              <div className="mt-2 sm:mt-0 bg-yellow-200">
+              <div className="mt-2 sm:mt-0 flex flex-col sm:flex-row">
+                <div
+                  className={
+                    "mb-2 sm:mr-3 sm:mb-0" +
+                    (tasksContext.filters.length ? "" : " hidden")
+                  }
+                >
+                  <Button
+                    buttonText="Clear filters"
+                    handleClick={handleClear}
+                  ></Button>
+                </div>
                 <Button buttonText="Search" handleClick={handleSearch}></Button>
               </div>
             </div>
