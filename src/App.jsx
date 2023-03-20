d3afc43d2cf4540157925d5efd92b62e97d7b9da
Aldo Santiago <aldo11santiago@gmail.com>
2023-03-20 11:52:33 -0600

    active filters label shows when active

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,49 @@ import Filters from "./components/Filters";
 import Table from "./components/Table";
 import Button from "./components/shared/Button";
 import PageNav from "./components/PageNav";
-import TasksProvider from "./store/TasksProvider";
 import TaskModal from "./components/TaskModal";
 import { useContext } from "react";
 import LayoutContext from "./store/layout-context";
+import TasksContext from "./store/tasks-context";
 
 function App() {
   const layoutContext = useContext(LayoutContext);
+  const tasksContext = useContext(TasksContext);
 
   return (
     <div className={"App" + (layoutContext.dark ? " dark " : " ")}>
-      {/* <div className="dark:bg-black">hola</div> */}
-      <TasksProvider>
-        <Layout>
-          <div className="md:w-5/6 lg:max-w-3xl mx-auto">
-            <Filters />
-          </div>
-          <div className="md:w-5/6 lg:max-w-3xl mx-auto">
-            <div className="my-4">
-              <Button
-                buttonText="New To-Do"
-                handleClick={() => layoutContext.doShowTaskModal()}
-              />
+      <Layout>
+        <div className="md:w-5/6 lg:max-w-3xl mx-auto">
+          <Filters />
+        </div>
+        <div className="md:w-5/6 lg:max-w-3xl mx-auto">
+          <div
+            className={
+              "my-4 relative" +
+              (tasksContext.filters.length ? " pb-10 sm:pb-0" : "")
+            }
+          >
+            <Button
+              buttonText="New To-Do"
+              handleClick={() => layoutContext.doShowTaskModal()}
+            />
+            <div
+              // ref={infoRef}
+              className={
+                "absolute bottom-0 left-0 right-0 w-fit -z-10 w-sm bg-gray-50 text-xs border px-2 mx-auto border-gray-400 rounded-full transition-all duration-1000" +
+                (tasksContext.filters.length ? " opacity-100" : " opacity-0")
+              }
+            >
+              Filters are active
             </div>
-            <Table />
           </div>
-          <div className="mt-4 md:w-5/6 lg:max-w-3xl mx-auto flex justify-center">
-            <PageNav />
-          </div>
-          {layoutContext.showTaskModal && <TaskModal />}
-        </Layout>
-      </TasksProvider>
+          <Table />
+        </div>
+        <div className="mt-4 md:w-5/6 lg:max-w-3xl mx-auto flex justify-center">
+          <PageNav />
+        </div>
+        {layoutContext.showTaskModal && <TaskModal />}
+      </Layout>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import LayoutProvider from "./store/LayoutProvider";
+import TasksProvider from "./store/TasksProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
+  <TasksProvider>
     <LayoutProvider>
       <App />
     </LayoutProvider>
+  </TasksProvider>
   // </React.StrictMode>
 );
